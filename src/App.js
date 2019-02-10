2311a3c5d8089eefa1d0f3bf1e33c986c870c1b4
Zubair Amjad <amjadz@uw.edu>
2019-02-09 22:34:08 -0800

    Got response from JSON

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
+import Search from './Search';
+
 import './App.css';
 
+var key = '973163c29a164868b20716a8d64a5851';
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+        articles: []
+    };
+}
+
+handleFormSubmit(item) {
+    var url = 'https://newsapi.org/v2/everything?q=' + item +'&from=2019-01-10&sortBy=publishedAt&apiKey=' + key;
+  
+    fetch(url)
+    .then((response) => {
+        console.log(response);
+        return response.json();
+
+    })
+    .then((json) => {
+      console.log(json)
+        this.setState({
+          
+            name: json.author,
+            title: json.title, 
+            desc: json.description,
+            url: json.url
+        });
+    })
+    .catch((error) => {
+      window.alert("Enter a real city please!");
+      this.setState({
+          name: null,
+          title: null,
+          desc: null,
+          url: null,
+      });
+  });
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <div className="moveItems">
-              <h1>Welcome!</h1>
-              <input placeholder="Search..."></input>
-              <button type="submit">Search</button>
-          </div>
+            <h1>Welcome!</h1>
+            <Search 
+                handleFormSubmit={(item) => {
+                this.handleFormSubmit(item); 
+                
+                }}/>
+
         </header>
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ src/Search.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class Search extends Component {
+    render(){
+        return(
+            <form onSubmit={(e) => this.handleFormSubmit(e)}>
+                    <input type="text" id="lookUp" placeholder="Search..." ref="searchBox" />
+                    <button className="btn btn-primary" type="submit" id="searchButton">Search</button>
+            </form>
+        )
+    }
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+        var item = this.refs.searchBox.value;
+        this.props.handleFormSubmit(item);
+    }
+
+}
+
+export default Search;
