ef9cdba2feae4e59db4a652c7abaeab0e3c17ab4
Zubair Amjad <amjadz@uw.edu>
2019-03-24 00:36:20 -0700

    Updates

---
+++ precache-manifest.9e6438f0795aa5960c0ad8a1112ef2ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eaa353a518d8660e339",
+    "url": "/sportsnews/static/css/main.392acd6e.chunk.css"
+  },
+  {
+    "revision": "5eaa353a518d8660e339",
+    "url": "/sportsnews/static/js/main.a1071bf3.chunk.js"
+  },
+  {
+    "revision": "12c9eb6e0b1323e94bc2",
+    "url": "/sportsnews/static/js/runtime~main.ce8ed10a.js"
+  },
+  {
+    "revision": "359276867f0c03a54c54",
+    "url": "/sportsnews/static/js/2.2008ee0e.chunk.js"
+  },
+  {
+    "revision": "c08536479752f7497967422c10583243",
+    "url": "/sportsnews/index.html"
+  }
+];
