2afc61e397f887c75dd3bce80720438bc60c3193
Zubair Amjad <amjadz@uw.edu>
2019-03-23 20:23:38 -0700

    Everything functionality wise works, going to style

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const ArticleList = props => {
     const articles = props.articles.map((article, description) => {
-        return <div>
-            <img src ={ article.urlToImage } alt={description} ></img>    
-            <h1>{article.title}</h1>      
-            <h1>{article.description}</h1>
-
-
+        return <div className = "card">
+                <img src ={ article.urlToImage } alt={description} ></img>    
+            <div className="card-body">
+                <h5 className="card-title">{article.title}</h5>      
+                <p className="card-text">{article.description}</p>
+                <a href={ article.url } class="btn btn-primary">Link to the Article</a>
+            </div>
         </div>
         
     })
     return(
         <div>{ articles }</div>
-
     )
 
 }
