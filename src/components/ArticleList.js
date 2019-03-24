3e8c4272b0ac0f7691e984a31decd220795f225b
Zubair Amjad <amjadz@uw.edu>
2019-03-24 00:30:27 -0700

    Version 1.0 complete

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import '../css/articlelist.css'
 
 const ArticleList = props => {
     const articles = props.articles.map((article, description) => {
         return <div className = "container fluid">
-            <div className="card mt-5">
-                <img src ={ article.urlToImage } alt={description} ></img>    
-                <div className="card-body">
-                    <h5 className="card-title">{article.title}</h5>      
-                    <p className="card-text">{article.description}</p>
-                    <a href={ article.url } class="btn btn-primary">Link to the Article</a>
+                <div className="card mt-5">
+                    <img src ={ article.urlToImage } alt={description} ></img>    
+                    <div className="card-body">
+                        <h5 className="card-title">{article.title}</h5>      
+                        <p className="card-text">{article.description}</p>
+                        <a href={ article.url } className="btn btn-primary">Link to the Article</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        
         
     })
     return(
